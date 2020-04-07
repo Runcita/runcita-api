cbcdf3d273f13a3354cbe9338da9ac45e325c470
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-04-07 16:54:02 +0200

    chore (init): import project

---
+++ src/main/java/com/runcita/runcitaapi/RuncitaApiApplication.java
@@ -0,0 +1,13 @@
+package com.runcita.runcitaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuncitaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuncitaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runcita/runcitaapi/RuncitaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runcita.runcitaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuncitaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
