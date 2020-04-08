5d4c5b05bdc7aaeb1fdbf98c02312a67f9609862
landchootl <ludovic.landschoot.partner@decathlon.com>
2020-04-09 00:44:01 +0200

    feat (test): add application properties in test context

--- src/main/java/com/runcita/api/ping/PingController.java
+++ src/main/java/com/runcita/api/ping/PingController.java
@@ -1,7 +1,5 @@
 package com.runcita.api.ping;
 
-import com.runcita.api.user.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PingController {
-
-    @Autowired
-    UserService userService;
 
     /**
      * Check if the server is started
